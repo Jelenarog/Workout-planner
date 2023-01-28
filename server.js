1ab804f5f0668ffc36a0fcb20c77070dc8326535
Jelenarog <jelena.rogic90@gmail.com>
2023-01-28 10:55:31 -0700

    user model and sessions

--- server.js
+++ server.js
@@ -1,37 +1,38 @@
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+
 const app = express();
-const PORT = process.env.PORT || 3008;
+const PORT = process.env.PORT || 3001;
 
+// Set up sessions with cookies
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    // Stored in milliseconds
+    maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
+app.use(session(sess));
 
+app.use(routes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// turn activate the connection to the DB and local host port server// 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, () =>
+      console.log(
+        `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
+      )
+    );
   });
