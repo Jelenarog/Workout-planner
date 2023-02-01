4ef0c0145de4479f83fb31dc99ed5cd799a103cf
Cody Cooper <cody_codster@live.com>
2023-02-01 01:42:01 -0600

    Added error handling to UI for registration page

--- public/js/register.js
+++ public/js/register.js
@@ -1,9 +1,20 @@
+const usernameErr = document.querySelector('.usernameError');
+const emailErr = document.querySelector('.emailError');
+const passwordErr = document.querySelector('.passwordError');
+const createSuccess = document.querySelector('.accCreated');
+
 const registerFormHandler = async (event) => {
     event.preventDefault(); 
 
     const username = document.querySelector('#usernameInput').value.trim();
     const email = document.querySelector('#emailInput').value.trim();
     const password = document.querySelector('#passwordInput').value.trim();
+
+    //Password length check
+    if(password.length < 8) {
+        passwordErr.innerHTML = 'Password must be at least 8 characters long!';
+        return;
+    }
 
     if (username && email && password) {
         const response = await fetch('/api/users/register', {
@@ -13,9 +24,14 @@ const registerFormHandler = async (event) => {
         });
 
         if (response.ok) {
-            document.location.replace('/login')
+            //Successful
+            createSuccess.innerHTML = 'Your account has been created!'
+            setTimeout(() => {
+                document.location.replace('/login')
+            }, 2000)
         } else {
-            alert('Failed to register');
+            usernameErr.innerHTML = 'Username already exists'
+            return;
         }
     }
 };
