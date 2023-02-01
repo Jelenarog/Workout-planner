24ae9eb0234433c2f8cdbe6538ea364e9b32d748
Cody Cooper <cody_codster@live.com>
2023-02-01 09:36:34 -0600

    Added scheduler feature to add exercises to schedule

--- public/js/addExercise.js
+++ public/js/addExercise.js
@@ -1,14 +1,49 @@
-const addExerciseBtn = document.querySelector('#addExercise');
-const reps = document.querySelector('#repsNum');
+const exerciseSelectBtn = document.querySelector('#exerciseSelect');
 const dateInput = $('#startDate');
-const exerciseId = document.querySelector('.exerciseCard');
+const setsInput = document.querySelector('#numSets');
+const repsInput = document.querySelector('#numReps');
+const weightInput = document.querySelector('#numWeight');
+const timeInput = document.querySelector('#numTime');
+const scheduleBtn = document.querySelector('#submitExerciseBtn');
 
+let exerciseId;
+let date;
+let sets;
+let reps;
+let weight;
+let time;
 
-const addExercise = () => {
-    const repsInput = reps.value;
-    const exerciseId = exerciseId.id;
-    const selectedDate = dateInput.val();
+const defaultHandler = () => {
+    scheduleBtn.classList.remove('bg-danger');
+    scheduleBtn.innerHTML = 'SUBMIT';
+};
+
+const scheduleExercise = async (e) => {
+    e.preventDefault();
+    exerciseId = exerciseSelectBtn.value;
+    date = dateInput.val();
+    sets = setsInput.value;
+    reps = repsInput.value;
+    weight = weightInput.value;
+    time = timeInput.value;
+    if (exerciseId && date) {
+        // const response = await fetch('api/users/schedule/add', {
+        //     method: 'POST',
+        //     body: JSON.stringify({exerciseId, date, sets, reps, weight, time}),
+        //     headers: {'Content-Type': 'application/json'}
+
+        // });
+
+        if(true) {
+            scheduleBtn.classList.add('bg-success');
+            scheduleBtn.innerHTML = 'SAVED!'
+            setTimeout(defaultHandler(),1300)
+        }
+    } else {
+            scheduleBtn.classList.add('bg-danger');
+            scheduleBtn.innerHTML = 'INSUFFICIENT DATA!'
+            setTimeout(defaultHandler(),1300)
+    }
 }
 
-
-addExerciseBtn.addEventListener('click', addExercise);
+scheduleBtn.addEventListener('click', scheduleExercise);
