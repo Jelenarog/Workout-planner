b12836f74fad12365d3bf8d4bdc2786ffe56f500
Cody Cooper <cody_codster@live.com>
2023-02-04 17:08:16 -0600

    delete favorite functionality

---
+++ public/js/deleteFavorite.js
@@ -0,0 +1,26 @@
+const deleteFavBtns = document.querySelectorAll('#deleteFavorite');
+
+const deleteFavorite = async(e) => {
+    e.preventDefault();
+        const exerciseId = parseInt(e.target.id);
+        console.log(exerciseId);
+        console.log(typeof exerciseId);
+        const response = await fetch('/api/favorite/delete', {
+            method: 'POST',
+            body: JSON.stringify ({ exerciseId }),
+            headers: { 'Content-Type': 'application/json'},
+        });
+
+        if(!response.ok) {
+            console.log('There has been an error deleting your favorite')
+            return;
+        } else {
+            document.location.reload();
+        };
+    };
+
+
+//Event Listeners
+for (deleteFav of deleteFavBtns) {
+    deleteFav.addEventListener('click', deleteFavorite);
+};
