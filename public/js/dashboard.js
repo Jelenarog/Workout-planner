369cf9469e27f7172353af3d89fdc80ca4ca1616
Cody Cooper <cody_codster@live.com>
2023-02-04 20:50:48 -0600

    Converted ISO date to UTC in dashboard.js file

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,7 +2,18 @@ const dashboardLink = document.querySelector('#dashboard-link');
 
 //Grabbing todays day an converting it to YYYY-MM-DD !Important: Need to update to local time zone
 const currentDate = new Date();
-const formattedDate = currentDate.toISOString().split('T')[0]; // *this needs to be changed to UTC*
+// const formattedDate = currentDate.toISOString().split('T')[0]; // *this needs to be changed to UTC*
+
+        let month = `${currentDate.getMonth()+1}`
+        let day = `${currentDate.getDate()}`;
+        let year = currentDate.getFullYear();
+
+    if (month.length < 2) 
+        month = `0${month}`;
+    if (day.length < 2) 
+        day = `0${day}`;
+
+    let formattedDate = [year, month, day].join('-');
 
 //Get request to controller home routes for dashboard url
 const showSchedule = () => {
